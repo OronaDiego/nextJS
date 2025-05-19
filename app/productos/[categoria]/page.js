823ca4da46891a0485196873a3d9eb9f92f5ac2e
Diego <diegosebastianorona@gmail.com>
2025-05-19 20:06:57 -0300

    Adding title dinamic

--- app/productos/[categoria]/page.js
+++ app/productos/[categoria]/page.js
@@ -1,17 +1,22 @@
 import Link from "next/link";
 // import { usePathname } from "next/navigation";
 
-export default async function Productos({params}){
+export async function generateMetadata({params, searchParams}, parent){
     const {categoria } = await params;
-    console.log(categoria);
-    
+    return{
+        title: categoria.toUpperCase().replaceAll("-","") + "| Tienda"
+    }
+};
+
+export default async function Productos({params}){    
+    const {categoria } = await params;
     // const ubicacion = usePathname();
     // console.log(ubicacion);
     
     return(
         <div className="flex justify-center items-center h-screen">
             <div className="text-center">
-                <h1 className="text-9xl">{categoria.toUpperCase()}</h1>
+                <h1 className="text-9xl">{categoria.toUpperCase().replaceAll("-"," ")}</h1>
                 <h3 className="text-3xl">Contenido de la pagina</h3>
                 <Link href={"/"}>Volver a la Pagina Principal</Link>
             </div>
