f42114e781cf402e058ffc52cf274ac6e754f4e2
Diego Oroná <diegosebastianorona@gmail.com>
2025-05-19 14:38:54 -0300

    adding details

--- app/productos/[categoria]/page.js
+++ app/productos/[categoria]/page.js
@@ -11,7 +11,7 @@ export default async function Productos({params}){
     return(
         <div className="flex justify-center items-center h-screen">
             <div className="text-center">
-                <h1 className="text-9xl">{categoria}</h1>
+                <h1 className="text-9xl">{categoria.toUpperCase()}</h1>
                 <h3 className="text-3xl">Contenido de la pagina</h3>
                 <Link href={"/"}>Volver a la Pagina Principal</Link>
             </div>
